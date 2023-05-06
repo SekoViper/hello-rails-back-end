6d449c216b8567e52fe356881312122c1e083c61
SekoViper <damehlivingstone1@gmail.com>
2023-05-06 16:43:30 +0000

    Add CORS.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001' # Update this with the domain and port of your frontend
+      origins 'http://localhost:3001'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end  
