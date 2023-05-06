5afd690d525140d186ad341224079e566b905319
SekoViper <damehlivingstone1@gmail.com>
2023-05-06 12:45:59 +0000

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-
-# Message.create(greeting: 'Hello, I am SekoViper.')
-# Message.create(greeting: 'Hi, I am SekoViper.')
-# Message.create(greeting: 'Hey, I am SekoViper.')
-# Message.create(greeting: 'Yo, I am SekoViper.')
-# Message.create(greeting: 'What\'s up?, I am SekoViper.')
+Message.create(greeting: 'Hello, I am SekoViper.')
+Message.create(greeting: 'Hi, I am SekoViper.')
+Message.create(greeting: 'Hey, I am SekoViper.')
+Message.create(greeting: 'Yo, I am SekoViper.')
+Message.create(greeting: 'What\'s up?, I am SekoViper.')
